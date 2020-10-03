86ca0ee3eafb1c172e3470697f45fd2be0c98623
LoluAlake <Temiloluwakomolafe@yahoo.com>
2020-10-03 08:11:38 -0400

    add and remove

--- src/main/java/com/dtcc/projects/PhoneBook.java
+++ src/main/java/com/dtcc/projects/PhoneBook.java
@@ -1,4 +1,30 @@
 package com.dtcc.projects;
 
+import java.util.*;
+
 public class PhoneBook {
+    private String name;
+    private String phoneNumber;
+
+    HashMap<String, String> PhoneBook = new HashMap<>();
+    Scanner input = new Scanner(System.in);
+
+    public PhoneBook(){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void add(String name, String phoneNumber){
+        PhoneBook.put(name, phoneNumber);
+    }
+
+    public void addAll(String name, String [] phoneNumbers){
+        List<String> phonenumbers = new ArrayList<>();
+
+      //  PhoneBook.putAll(phoneNumbers);
+    }
+
+    public void remove(String name){
+        PhoneBook.remove(name);
+    }
 }
