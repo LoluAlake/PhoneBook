24655952dba88dfd6bb35b4a0e7c40be5efab145
LoluAlake <Temiloluwakomolafe@yahoo.com>
2020-10-03 13:04:01 -0400

    update

--- src/main/java/com/dtcc/projects/PhoneBook.java
+++ src/main/java/com/dtcc/projects/PhoneBook.java
@@ -5,26 +5,53 @@ import java.util.*;
 public class PhoneBook {
     private String name;
     private String phoneNumber;
+    private int numbers;
 
-    HashMap<String, String> PhoneBook = new HashMap<>();
+    Map<String, List<String>> phoneBookMap;
     Scanner input = new Scanner(System.in);
 
     public PhoneBook(){
-        this.name = name;
-        this.phoneNumber = phoneNumber;
+     this(new LinkedHashMap<String, List<String>>());
     }
 
-    public void add(String name, String phoneNumber){
-        PhoneBook.put(name, phoneNumber);
+    public PhoneBook(Map<String, List<String>> map){
+        this.phoneBookMap = map;
     }
 
-    public void addAll(String name, String [] phoneNumbers){
-        List<String> phonenumbers = new ArrayList<>();
-
-      //  PhoneBook.putAll(phoneNumbers);
+    public void add(String name, String phoneNumber) {
+        List<String> phoneNumberList = phoneBookMap.get(name);
+        if (phoneNumberList != null) {
+            phoneNumberList.add(phoneNumber);
+        } else {
+            phoneBookMap.put(name, Collections.singletonList(phoneNumber));
+        }
     }
 
+    public void addAll(String name, String[] phoneNumbers){
+        phoneBookMap.put(name, Arrays.asList(phoneNumbers));
+    }
     public void remove(String name){
-        PhoneBook.remove(name);
+        phoneBookMap.remove(name);
+    }
+
+    public boolean hasEntry(String name){
+        return phoneBookMap.containsKey(name);
+    }
+
+    public List<String> lookup(String name){
+        return phoneBookMap.get(name);
+    }
+
+    public String reverseLookup(String phoneNumber){
+        phoneBookMap.get(phoneNumber);
+        return phoneNumber;
+    }
+
+    public String getAllContactNames(){
+        List<String> phoneBook = new ArrayList<>();
+        for(int i = 0; i < phoneBook.size(); i++){
+           phoneBookMap.get(phoneBook.get(i));
+        }
+        return name;
     }
 }
--- src/test/java/com/dtcc/projects/PhoneBookTest.java
+++ src/test/java/com/dtcc/projects/PhoneBookTest.java
@@ -1,4 +1,104 @@
 package com.dtcc.projects;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
 public class PhoneBookTest {
+    @Test
+    public void addTest(){
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "Lolu";
+        String phoneNumber = "2405793527";
+        phoneBook.add(name, phoneNumber);
+        Assert.assertTrue(phoneBook.phoneBookMap.containsKey(name));
+        Assert.assertEquals(phoneNumber, phoneBook.phoneBookMap.get(name).get(0));
+    }
+    @Test
+    public void addAllTest(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "Charu";
+        String[] strNum = {"357980865", "446789908", "3456677878"};
+        //When
+        phoneBook.addAll(name, strNum);
+        //Then
+        Assert.assertEquals(Arrays.asList(strNum), phoneBook.phoneBookMap.get(name));
+    }
+
+    @Test
+    public void removeTest(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "Lolu";
+        String phoneNumber = "2405793527";
+        phoneBook.add(name, phoneNumber);
+
+        String name1 = "mide";
+        String phoneNumber1 = "2405794567";
+        phoneBook.add(name1, phoneNumber1);
+
+        //When
+        phoneBook.remove("mide");
+        //Then
+        Assert.assertFalse(phoneBook.phoneBookMap.containsKey("mide"));
+    }
+
+    @Test
+    public void hasEntryTest(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "Lolu";
+        String phoneNumber = "2405793527";
+        phoneBook.add(name, phoneNumber);
+
+        String name1 = "mide";
+        String phoneNumber1 = "2405794567";
+        phoneBook.add(name1, phoneNumber1);
+
+        //When
+        phoneBook.hasEntry("Lolu");
+        //Then
+        Assert.assertTrue(phoneBook.phoneBookMap.containsKey("Lolu"));
+    }
+
+    @Test
+    public void lookupTest(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "Lolu";
+        String phoneNumber ="357980865";
+        phoneBook.add(name, phoneNumber);
+
+        phoneBook.lookup("Lolu");
+        Assert.assertEquals(Arrays.asList(phoneNumber), phoneBook.phoneBookMap.get(name));
+    }
+
+    @Test
+    public void reverseLookupTest(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "Lolu";
+        String phoneNumber ="357980865";
+        phoneBook.add(name, phoneNumber);
+
+        phoneBook.lookup("Lolu");
+        Assert.assertEquals(Arrays.asList(phoneNumber), phoneBook.phoneBookMap.get(name));
+    }
+
+    @Test
+    public void getAllContactName(){
+        PhoneBook book = new PhoneBook();
+        String name = "Lolu";
+        String phoneNumber = "2405793527";
+        book.add(name, phoneNumber);
+
+        String name1 = "mide";
+        String phoneNumber1 = "2405794567";
+        book.add(name1, phoneNumber1);
+        book.getAllContactNames();
+
+        
+    }
 }
